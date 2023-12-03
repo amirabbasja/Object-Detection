b09adb791f34191562668a724a12a32c49a079bc
abbas <jafarpourabbas@googlemail.com>
2023-12-03 13:06:28 +0330

    Minor changes

--- dataHandler.py
+++ dataHandler.py
@@ -4,8 +4,10 @@ Contains the necessary function for handeling the train, test and cross-validati
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg # Necessary for readig an image
 import matplotlib.patches as patches # Necessary for drawing bounding boxes
+import glob
 import json
-
+from pathlib import Path
+import pandas as pd
 
 def dispBBox(picDir, picName, labelDetail, labelsNames):
     """
@@ -60,3 +62,76 @@ def dispBBox(picDir, picName, labelDetail, labelsNames):
             )
     
     return None
+
+def annotationsToDataframe(annotDir, annotExt, annotId = None):
+    """
+    Reads the annotations from a directory and returns a dataframe. Annotations can be saved
+    in two formats: TXT or XLS. 
+        TXT: The annotations saved in text files should contain a single detection in each 
+        line. Every line should be in the following order: [className rel_x rel_y width height].
+        Where rel_x and rel_y are the coordinates of the center of the bounding box relative to 
+        the entire picture and width and height of the box are also relative the entre picture.
+        XLS: ----TODO----
+    Note that it is assumed that the ID of each annotation is the file's name and there should 
+    be and image file with the same exact name (And different extension) in the data directory.  
+
+    Args:
+        annotDir: str: The directory containing annotations.
+        annotExt: str: The extensions of the annotations.
+        annotId: str: The id of the specific image. If you want the returned dataframe to contain only 
+            the annotations for a specific image. If None, the entire annotation directory will 
+            be read and returned as a dataframe.
+
+     Returns: 
+        A pandas dataframe with columns: [id, boxCenterX, boxCenterY, boxWidth, boxHeight, class]       
+    """
+    # For performance purposes, we wont use append/concat row methods for each new entry. We append 
+    # new data to lists as we iterate through the annotations. At the end we make a dataframe with 
+    # the lists in hand.Temprorary lists for appending the new data.
+    __lstID = []
+    __lstBoxCenterX = []
+    __lstBoxCenterY = []
+    __lstBoxWidth = []
+    __lstBoxHeight = []
+    __lstClass = []
+
+    if annotExt.lower() == "txt":
+        # Read the files in the directory
+        files = glob.glob(f"{annotDir}/*.txt")
+        for file in files:
+
+            # GEt the annotation ID which is the file name
+            __fName = Path(file).stem
+            
+            # Only get a specific annotation. IF else, get all the annotations.
+            if annotId != None:
+                if __fName != annotId:
+                    continue
+
+            with open(file) as f:
+                
+                for annot in f.readlines():
+                    annot = annot.replace("\n","") # Replace newline character
+                    annot = annot.split(" ")
+
+                    # Append the new data
+                    __lstID.append(__fName)
+                    __lstBoxCenterX.append(float(annot[1]))
+                    __lstBoxCenterY.append(float(annot[2]))
+                    __lstBoxWidth.append(float(annot[3]))
+                    __lstBoxHeight.append(float(annot[4]))
+                    __lstClass.append(int(annot[0]))
+        
+    elif annotExt.lower() == "txt":
+        pass
+    else:
+        print("Invalid extension type. Only text and XML files are acceptable.")
+
+    
+    # Merge the lists to make a dataframe
+    df = pd.DataFrame(
+        list(zip(__lstID, __lstBoxCenterX, __lstBoxCenterY, __lstBoxWidth, __lstBoxHeight, __lstClass)),
+        columns = ["id", "boxCenterX", "boxCenterY", "boxWidth", "boxHeight", "class"]   
+    )
+
+    return df
